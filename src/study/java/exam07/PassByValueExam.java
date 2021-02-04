6563cb58d31dcf495cb64ded0ac231d519b2bef2
OGAREE <ohkangun@naver.com>
2021-02-04 13:48:35 +0900

    PassByValue예시

---
+++ src/study/java/exam07/PassByValueExam.java
@@ -0,0 +1,12 @@
+package study.java.exam07;
+
+public class PassByValueExam {
+	public static void main(String[] args) {
+		// pass by value 예시 1
+		int a = 10;
+		int b = a + 20;
+		a=b;
+		System.out.println(a);
+	}
+
+}
